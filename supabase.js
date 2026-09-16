d676e6862e8ddc7b1adcb7bd9b2e1c820d1e0730
fercho-2008-pixel <ferchogarces2008@gmail.com>
2026-09-16 16:26:59 -0500

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://vvvveahvvabpzkephwlu.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2dnZlYWh2dmFicHprZXBod2x1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0MDQzNzIsImV4cCI6MjEwNDk4MDM3Mn0.AXo3MSol4K7hawxwHsgHlEThGXzZZn4u4WdMXH7k2ts';
+
+
+export const db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
